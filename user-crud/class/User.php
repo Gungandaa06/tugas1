8f6efc44bfa04ab5adf466b33e57afa4aadd5447
Gungandaa06 <gungnandamahesya@gmail.com>
2025-12-03 21:45:06 +0800

    Update User.php

--- user-crud/class/User.php
+++ user-crud/class/User.php
@@ -6,7 +6,6 @@ public function __construct(PDO $pdo) {
 $this->pdo = $pdo;
 }
 
-
 // contoh: cek login (username/password simple, password hashed)
 public function authenticate($username, $password) {
 $stmt = $this->pdo->prepare('SELECT id, username, password_hash FROM users WHERE username = :u LIMIT 1');

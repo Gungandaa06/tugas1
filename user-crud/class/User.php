2d6cdebd923391daf5cdf67d94a3f69df456c05c
Gungandaa06 <gungnandamahesya@gmail.com>
2025-12-03 21:44:33 +0800

    Update User.php

--- user-crud/class/User.php
+++ user-crud/class/User.php
@@ -1,69 +1,20 @@
 <?php
-
+// class/User.php
 class User {
-
-  
-// protected properties
-protected $id;
-protected $password;
-protected $db;
-
-  // protected properties
-protected $id;
-protected $password;
-protected $db;
-
-  // constructor
-
-public function __construct() {
-  $this->db = new Database();
+private $pdo;
+public function __construct(PDO $pdo) {
+$this->pdo = $pdo;
 }
 
-  // Authenticate user credentials
-  public function __construct() {
-    
-  }
 
-  // Get all users
-  puiblic function getAll(){
-    $sql = "SELECT"
-  }
-
-  // Set user properties by ID
-
-public function setById($id) {
-  $sql = "SELECT * FROM users WHERE id = :id LIMIT 1";
-  $stmt = $this->db->query($sql, ['id' => $id]);
-  $user = $stmt->fetch(PDO::FETCH_ASSOC);
-
-  if ($user) {
-    $this->id = $user['id'];
-    $this->username = $user['username'];
-    $this->fullname = $user['fullname'];
-    $this->city = $user['city'];
-    $this->created_at = $user['created_at'];
-    $this->password = $user['password'];
-    return true;
-  }
-  return false;
+// contoh: cek login (username/password simple, password hashed)
+public function authenticate($username, $password) {
+$stmt = $this->pdo->prepare('SELECT id, username, password_hash FROM users WHERE username = :u LIMIT 1');
+$stmt->execute([':u' => $username]);
+$row = $stmt->fetch();
+if ($row && password_verify($password, $row['password_hash'])) {
+return $row;
 }
-
-  // Get ID
-  public function getID()
-    return $this->id;
-
-  // Get password
- public function getPassword(){
-  return $this->password;
- }
-
-  // Set password (hashed)
-  public function setPass($password){
-    sthis->password = password_hash ($password, PASSWORD_DEFAULT);
-  }
-
-  // Save user (insert or update)
-  
-  // Remove user
-  
+return false;
+}
 }
